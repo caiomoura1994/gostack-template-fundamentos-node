c695c37aaa055f43ee6e06f6faa1d5711977f06c
Caio Moura <caiomoura1994@gmail.com>
2022-11-13 10:41:32 -0300

    Handle with transaction routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,25 +1,33 @@
 import { Router } from 'express';
+import CreateTransactionService from '../services/CreateTransactionService';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
-transactionRouter.get('/', (request, response) => {
+transactionRouter.get('/', (_request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all()
+    const balance = transactionsRepository.getBalance()
+    return response.json({ transactions, balance });
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    if (err instanceof Error)
+      return response.status(400).json({ error: err.message });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const transactionBodyData: Omit<Transaction, 'id'> = request.body
+    const newTransaction = new CreateTransactionService(transactionsRepository)
+    const transactionResponse = newTransaction.execute(transactionBodyData)
+    return response.json(transactionResponse);
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    if (err instanceof Error)
+      return response.status(400).json({ error: err.message });
   }
 });
 
